12166a39293d7a799abc1f8f3214a627ffcb6e6b
NoelJasik <Noeloskar99@gmail.com>
2022-12-12 00:17:50 +0100

    Initial commit

--- index.js
+++ index.js
@@ -17,105 +17,132 @@ fs.readdir(dir, (err, files) => {
 
     // Create a repository for each subdirectory
     files.forEach((file) => {
-        const repoName = file;
+        if (!file.endsWith(".zip")) {
+            const repoName = file;
 
-        const gitignoreContents = "node_modules/\n.DS_Store"; // replace with your desired .gitignore file contents
+            const gitignoreContents = "node_modules/\n.DS_Store"; // replace with your desired .gitignore file contents
 
-           // Set the local directory to push to the repository
-           const localDir = `${dir}/${repoName}`;
+            // Set the local directory to push to the repository
+            const localDir = `${dir}/${repoName}`;
 
-        const gitignorePath = `${localDir}/.gitignore`;
+            const gitignorePath = `${localDir}/.gitignore`;
 
-        // Write the .gitignore file
-        fs.readFile('./gitignore.txt', "utf8", (err, data) => {
-            if (err) {
-              console.error(`Error reading .gitignore file: ${err}`);
-              return;
-            }
-          
-            // Set the gitignoreContents variable to the contents of the file
-            const gitignoreContents = data;
-          
             // Write the .gitignore file
-            fs.writeFile(gitignorePath, gitignoreContents, (err) => {
-              if (err) {
-                console.error(`Error writing .gitignore file: ${err}`);
-                return;
-              }
-          
-              console.log(`Successfully wrote .gitignore file to ${gitignorePath}`);
-            });
-          });
+            fs.readFile('./gitignore.txt', "utf8", (err, data) => {
+                if (err) {
+                    console.error(`Error reading .gitignore file: ${err}`);
+                    return;
+                }
 
-        // Set the repository options
-        const options = {
-            url: 'https://api.github.com/user/repos',
-            method: 'POST',
-            headers: {
-                'User-Agent': username,
-                'Authorization': `token ${token}`,
-            },
-            json: {
-                'name': repoName, // replace with your repository name
-                'description': 'Automatically generated repo from my harddrive', // replace with your repository description
-                'private': false, // set to true to create a private repository
-            },
-        };
+                // Set the gitignoreContents variable to the contents of the file
+                const gitignoreContents = data;
 
-
-
-        // Send the request to create the repository
-        request(options, (error, response, body) => {
-            if (!error && response.statusCode == 201) {
-                console.log(`Successfully created repository: ${body.html_url}`);
-
-                // Set the clone URL for the repository
-                const cloneUrl = body.clone_url;
-
-                // Set the local directory to push to the repository
-                const localDir = `${dir}/${repoName}`;
-
-                exec(`git init`, { cwd: localDir }, (err, stdout, stderr) => {
+                // Write the .gitignore file
+                fs.writeFile(gitignorePath, gitignoreContents, (err) => {
                     if (err) {
-                        console.error(`Error initializing Git repository: ${err}`);
+                        console.error(`Error writing .gitignore file: ${err}`);
                         return;
                     }
 
-                    // The Git repository was successfully initialized
-                    console.log(`Successfully initialized Git repository in ${localDir}`);
+                    console.log(`Successfully wrote .gitignore file to ${gitignorePath}`);
+                });
+            });
 
-                    // Push the local directory to the repository
-                    exec(`git remote add origin ${cloneUrl}`, { cwd: localDir }, (err, stdout, stderr) => {
+            // Set the repository options
+            const options = {
+                url: 'https://api.github.com/user/repos',
+                method: 'POST',
+                headers: {
+                    'User-Agent': username,
+                    'Authorization': `token ${token}`,
+                },
+                json: {
+                    'name': repoName, // replace with your repository name
+                    'description': 'Automatically generated repo from my harddrive', // replace with your repository description
+                    'private': true, // set to true to create a private repository
+                },
+            };
+
+
+
+            // Send the request to create the repository
+            request(options, (error, response, body) => {
+                if (!error && response.statusCode == 201) {
+                    console.log(`Successfully created repository: ${body.html_url}`);
+
+                    // Set the clone URL for the repository
+                    const cloneUrl = body.clone_url;
+
+                    // Set the local directory to push to the repository
+                    const localDir = `${dir}/${repoName}`;
+
+                    exec(`git init`, { cwd: localDir }, (err, stdout, stderr) => {
                         if (err) {
-                            console.error(`Error pushing to repository: ${err}`);
+                            console.error(`Error initializing Git repository: ${err}`);
                             return;
                         }
 
-                        exec(`git add .`, { cwd: localDir }, (err, stdout, stderr) => {
+                        // The Git repository was successfully initialized
+                        console.log(`Successfully initialized Git repository in ${localDir}`);
+
+                        // Push the local directory to the repository
+                        exec(`git remote add origin ${cloneUrl}`, { cwd: localDir }, (err, stdout, stderr) => {
                             if (err) {
                                 console.error(`Error pushing to repository: ${err}`);
                                 return;
                             }
 
-                            exec(`git commit -m "Initial commit"`, { cwd: localDir }, (err, stdout, stderr) => {
+                            exec(`git add .`, { cwd: localDir }, (err, stdout, stderr) => {
                                 if (err) {
                                     console.error(`Error pushing to repository: ${err}`);
                                     return;
                                 }
-                                exec(`git push -u origin master`, { cwd: localDir }, (err, stdout, stderr) => {
+
+                                exec(`git commit -m "Initial commit"`, { cwd: localDir }, (err, stdout, stderr) => {
                                     if (err) {
                                         console.error(`Error pushing to repository: ${err}`);
                                         return;
                                     }
+                                    exec(`git push -u origin master`, { cwd: localDir }, (err, stdout, stderr) => {
+                                        if (err) {
+                                            console.error(`Error pushing to repository: ${err}`);
+                                            return;
+                                        }
 
-                                    // The local directory was successfully pushed to the repository
-                                    console.log('Successfully pushed local directory to repository.');
+                                        // The local directory was successfully pushed to the repository
+                                        console.log('Successfully pushed local directory to repository.');
+                                    });
                                 });
                             });
                         });
+                    })
+                } else if (error == null) {
+
+                    exec(`git add .`, { cwd: localDir }, (err, stdout, stderr) => {
+                        if (err) {
+                            console.error(`Error pushing to repository: ${err}`);
+                            return;
+                        }
+
+                        exec(`git commit -m "Initial commit"`, { cwd: localDir }, (err, stdout, stderr) => {
+                            if (err) {
+                                console.error(`Error pushing to repository: ${err}`);
+                                return;
+                            }
+                            exec(`git push -u origin master`, { cwd: localDir }, (err, stdout, stderr) => {
+                                if (err) {
+                                    console.error(`Error pushing to repository: ${err}`);
+                                    return;
+                                }
+
+                                // The local directory was successfully pushed to the repository
+                                console.log('Successfully pushed local directory to repository.');
+                            });
+                        });
                     });
-                })
-            } else { console.log(error) }
-        })
-    })
+
+                }
+            })
+        }
+     })
 });
